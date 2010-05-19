1d8e44234627c271fad99e3aea0c9e0c203aa31b
turco <turco@b0b93765-f1f9-fdba-7a1c-48fe7a44355a>
2010-05-19 12:02:06 +0000

    Se elimino codigo muerto

--- projects/ar.uba.dc.thesis/src/main/java/ar/uba/dc/thesis/atam/ResponseMeasure.java
+++ projects/ar.uba.dc.thesis/src/main/java/ar/uba/dc/thesis/atam/ResponseMeasure.java
@@ -2,7 +2,6 @@ package ar.uba.dc.thesis.atam;
 
 import org.sa.rainbow.scenario.model.RainbowModelWithScenarios;
 
-import ar.uba.dc.thesis.common.Heuristic;
 import ar.uba.dc.thesis.common.ThesisPojo;
 import ar.uba.dc.thesis.rainbow.constraint.Constraint;
 
@@ -13,10 +12,6 @@ public class ResponseMeasure extends ThesisPojo {
 	private final Constraint constraint;
 
 	public ResponseMeasure(String description, Constraint constraint) {
-		this(description, constraint, Heuristic.MOST);
-	}
-
-	public ResponseMeasure(String description, Constraint constraint, Heuristic heuristic) {
 		super();
 		this.description = description;
 		this.constraint = constraint;
