0c794a622524e838375c4a7e15b63e39c2528abb
VisualSVN Server <VisualSVN Server@b0b93765-f1f9-fdba-7a1c-48fe7a44355a>
2010-06-15 01:11:23 +0000

    Deleted folder '/org.sa.rainbow.console'.

--- org.sa.rainbow.console/RainbowConsole-compile.cmd
+++
@@ -1,1 +0,0 @@
-@java -cp "%~dp0\src;%~dp0\bin;C:/server/google/gwt-windows-1.3.3/gwt-user.jar;C:/server/google/gwt-windows-1.3.3/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %* org.sa.rainbow.RainbowConsole
