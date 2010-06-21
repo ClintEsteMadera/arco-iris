47619d7bbfa8e049bff10534df253c1e2720006b
turco <turco@b0b93765-f1f9-fdba-7a1c-48fe7a44355a>
2010-06-21 14:45:26 +0000

    Se agregan aliases a xstream para poder parsear el xml de escenarios.

--- projects/ar.uba.dc.thesis/src/main/java/ar/uba/dc/thesis/atam/scenario/persist/SelfHealingScenarioPersister.java
+++ projects/ar.uba.dc.thesis/src/main/java/ar/uba/dc/thesis/atam/scenario/persist/SelfHealingScenarioPersister.java
@@ -7,9 +7,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import ar.uba.dc.thesis.dao.SelfHealingScenarioDao;
-import ar.uba.dc.thesis.dao.TestEnvironmentDao;
-import ar.uba.dc.thesis.dao.TestSelfHealingScenarioDao;
+import ar.uba.dc.thesis.rainbow.constraint.numerical.NumericBinaryRelationalConstraint;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.XStreamException;
@@ -27,25 +25,6 @@ public class SelfHealingScenarioPersister {
 		this.initXStream();
 	}
 
-	public SelfHealingConfiguration readFromFile(String scenariosInXmlFullPath) {
-		try {
-			forceClassLoading();
-
-			String scenariosInXml = FileUtils.readFileToString(new File(scenariosInXmlFullPath), CHARSET);
-			SelfHealingConfiguration configReadFromFile = (SelfHealingConfiguration) this.xstream
-					.fromXML(scenariosInXml);
-			logger.info("Scenarios successfully loaded from " + scenariosInXmlFullPath);
-
-			return configReadFromFile;
-		} catch (IOException e) {
-			throw new RuntimeException("Cannot load Scenarios from " + scenariosInXmlFullPath, e);
-		} catch (XStreamException xstreamException) {
-			logger.error("Error while unmarshaling scenarios");
-			throw xstreamException;
-
-		}
-	}
-
 	public void saveToFile(SelfHealingConfiguration selfHealingConfig, String fileFullPath) {
 		String xml = this.xstream.toXML(selfHealingConfig);
 		try {
@@ -56,16 +35,34 @@ public class SelfHealingScenarioPersister {
 		}
 	}
 
+	public SelfHealingConfiguration readFromFile(String scenariosInXmlFullPath) {
+		try {
+			logger.info("Loading Scenarios from " + scenariosInXmlFullPath + "...");
+			String scenariosInXml = FileUtils.readFileToString(new File(scenariosInXmlFullPath), CHARSET);
+			logger.info("Scenarios successfully loaded from " + scenariosInXmlFullPath);
+			return (SelfHealingConfiguration) this.xstream.fromXML(scenariosInXml);
+		} catch (IOException e) {
+			throw new RuntimeException("Cannot load Scenarios from " + scenariosInXmlFullPath, e);
+		} catch (XStreamException xstreamException) {
+			logger.error("Error while unmarshaling scenarios");
+			throw xstreamException;
+
+		}
+	}
+
 	private void initXStream() {
 		this.xstream = new XStream();
 		this.xstream.autodetectAnnotations(true);
+		addAlias(SelfHealingConfiguration.class);
+		addAlias(NumericBinaryRelationalConstraint.class);
 	}
 
-	private void forceClassLoading() {
-		SelfHealingScenarioDao dao = new TestSelfHealingScenarioDao(new TestEnvironmentDao());
-		SelfHealingConfiguration configToMarshal = new SelfHealingConfiguration(dao.getAllScenarios());
+	private void addAlias(Class<?> aClass) {
+		this.xstream.alias(classToAlias(aClass), aClass);
+	}
 
-		this.saveToFile(configToMarshal, "C:\\tmp.xml");
-
+	private String classToAlias(Class<?> aClass) {
+		String simpleName = aClass.getSimpleName();
+		return simpleName.substring(0, 1).toLowerCase() + simpleName.substring(1);
 	}
 }
