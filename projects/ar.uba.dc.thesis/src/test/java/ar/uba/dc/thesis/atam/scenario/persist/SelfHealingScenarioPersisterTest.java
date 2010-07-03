c38e67eac1ff02972cb694f0059772c762901ee9
Jonathan Chiocchio <jchiocchio@users.noreply.github.com>
2010-07-03 00:52:42 +0000

    clean up de warnings al pedo

--- projects/ar.uba.dc.thesis/src/test/java/ar/uba/dc/thesis/atam/scenario/persist/SelfHealingScenarioPersisterTest.java
+++ projects/ar.uba.dc.thesis/src/test/java/ar/uba/dc/thesis/atam/scenario/persist/SelfHealingScenarioPersisterTest.java
@@ -1,10 +1,6 @@
 package ar.uba.dc.thesis.atam.scenario.persist;
 
 import junit.framework.TestCase;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import ar.uba.dc.thesis.dao.SelfHealingScenarioDao;
 import ar.uba.dc.thesis.dao.TestEnvironmentDao;
 import ar.uba.dc.thesis.dao.TestSelfHealingScenarioDao;
@@ -12,8 +8,6 @@ import ar.uba.dc.thesis.dao.TestSelfHealingScenarioDao;
 public class SelfHealingScenarioPersisterTest extends TestCase {
 
 	private static final String SCENARIOS_IN_XML_FULL_PATH = "src/test/resources/testScenarios.xml";
-
-	private static final Log logger = LogFactory.getLog(SelfHealingScenarioPersisterTest.class);
 
 	/**
 	 * Tests the whole marshaling / unmarshaling process
