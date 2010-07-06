ef6be7bfd90742431fcd5655d9875fc019b0303d
gabriel <gtursi@users.noreply.github.com>
2010-07-06 03:23:58 +0000

    Se agregó la generacion del grafico del experRespTime como parte de la Simulacion

---
+++ projects/org.sa.rainbow.core/src-sim/org/sa/rainbow/monitor/sim/graphics/ClassResourceLoader.java
@@ -0,0 +1,39 @@
+package org.sa.rainbow.monitor.sim.graphics;
+
+import java.io.InputStream;
+
+import org.apache.commons.collections.ExtendedProperties;
+import org.apache.velocity.runtime.resource.Resource;
+import org.apache.velocity.runtime.resource.loader.ResourceLoader;
+
+/**
+ * Carga templates en forma relativa a esta clase.
+ * 
+ * @author H. Adrián Uribe
+ */
+public class ClassResourceLoader extends ResourceLoader {
+
+	@Override
+	public long getLastModified(Resource resource) {
+		return 0;
+	}
+
+	@Override
+	public InputStream getResourceStream(String name) {
+		return CLASS.getResourceAsStream(DIR.concat(name));
+	}
+
+	@Override
+	public void init(ExtendedProperties extendedproperties) {
+		// NOTA: Vacío
+	}
+
+	@Override
+	public boolean isSourceModified(Resource resource) {
+		return false;
+	}
+
+	private static final Class CLASS = ClassResourceLoader.class;
+
+	private static final String DIR = "templates/";
+}
