2c82fcce6d9982f3784a1c93b4661aba2d69078c
turco <turco@b0b93765-f1f9-fdba-7a1c-48fe7a44355a>
2010-07-18 18:40:07 +0000

    Se separo CONCERN_STILL_BROKEN en RESP_TIME_STILL_BROKEN y COST_STILL_BROKEN

--- projects/ar.uba.dc.thesis/configs/znewsWithScenarios3/stitch/newssiteStrategies.s
+++ projects/ar.uba.dc.thesis/configs/znewsWithScenarios3/stitch/newssiteStrategies.s
@@ -7,26 +7,9 @@ module newssite.strategies;
 import lib "newssiteTactics.s";
 
 define boolean styleApplies = Model.hasType(M, "ClientT") && Model.hasType(M, "ServerT");
-define boolean cViolation = exists c : T.ClientT in M.components | c.experRespTime > M.MAX_RESPTIME;
 
-define set servers = {select s : T.ServerT in M.components | true};
-define set unhappyClients = {select c : T.ClientT in M.components | c.experRespTime > M.MAX_RESPTIME};
-define int numClients = Set.size({select c : T.ClientT in M.components | true});
-define int numUnhappy = Set.size(unhappyClients);
-define float numUnhappyFloat = 1.0*numUnhappy;
-
-define boolean hiLoad = exists s : T.ServerT in M.components | s.load > M.MAX_UTIL;
-define boolean hiRespTime = exists c : T.ClientT in M.components | c.experRespTime > M.MAX_RESPTIME;
-define boolean lowRespTime = exists c : T.ClientT in M.components | c.experRespTime < M.MIN_RESPTIME;
-
-define float totalCost = Model.sumOverProperty("cost", servers);
-define boolean hiCost = totalCost >= M.THRESHOLD_COST;
-
-/*TO-DO: agregar logica en el ResponseMeasure para que soporte esto*/
-define float avgFidelity = Model.sumOverProperty("fidelity", servers) / Set.size(servers);
-define boolean lowFi = avgFidelity < M.THRESHOLD_FIDELITY;
-
-define boolean CONCERN_STILL_BROKEN = AdaptationManagerWithScenarios.isConcernStillBroken("RESPONSE_TIME");
+define boolean RESP_TIME_STILL_BROKEN = AdaptationManagerWithScenarios.isConcernStillBroken("RESPONSE_TIME");
+define boolean COST_STILL_BROKEN = AdaptationManagerWithScenarios.isConcernStillBroken("SERVER_COST");
 
 /* This Strategy is simple in that, while it encounters any anomaly in
  * experienced response time, it firsts enlists one new server, then lowers
@@ -37,8 +20,8 @@ define boolean CONCERN_STILL_BROKEN = AdaptationManagerWithScenarios.isConcernSt
 strategy EnlistServersResponseTime
 [ styleApplies ] {
   t0: (true) -> enlistServers(1) @[5000 /*ms*/] {
-	  t1: (!CONCERN_STILL_BROKEN) -> done;
-	  t2: (true) -> enlistServers(1) @[5000 /*ms*/] {
+	  t1: (!RESP_TIME_STILL_BROKEN) -> done;
+	  t2: (RESP_TIME_STILL_BROKEN) -> enlistServers(1) @[5000 /*ms*/] {
 		t3: (default) -> done;
 	  }
 	  t4: (default) -> TNULL;
@@ -52,8 +35,8 @@ strategy EnlistServersResponseTime
  */
 strategy ReduceOverallCost
 [ styleApplies ] {
-  t0: (true) -> dischargeServers(1) @[2000 /*ms*/] {
-    t1: (!CONCERN_STILL_BROKEN) -> done;
+  t0: (true) -> dischargeServers(1) @[5000 /*ms*/] {
+    t1: (!COST_STILL_BROKEN) -> done;
     t2: (default) -> TNULL;
   }
 }
