15d7e2c4b837fabf1443d727d85315c036e459ff
Jonathan Chiocchio <jchiocchio@users.noreply.github.com>
2010-07-27 23:21:48 +0000

    elimino referencias a Caja de Valores

--- projects/ar.uba.dc.thesis.scenariosUI/src/scenariosui/gui/widget/dialog/EnvironmentDialog.java
+++ projects/ar.uba.dc.thesis.scenariosUI/src/scenariosui/gui/widget/dialog/EnvironmentDialog.java
@@ -1,19 +1,3 @@
-/*
- * Licencia de Caja de Valores S.A., Versión 1.0
- *
- * Copyright (c) 2006 Caja de Valores S.A.
- * 25 de Mayo 362, Ciudad Autónoma de Buenos Aires, República Argentina
- * Todos los derechos reservados.
- *
- * Este software es información confidencial y propietaria de Caja de Valores S.A. ("Información
- * Confidencial"). Usted no divulgará tal Información Confidencial y la usará solamente de acuerdo a
- * los términos del acuerdo de licencia que posee con Caja de Valores S.A.
- */
-
-/*
- * $Id: ActualizarJornadaDialog.java,v 1.2 2008/05/14 19:56:20 cvschioc Exp $
- */
-
 package scenariosui.gui.widget.dialog;
 
 import org.eclipse.swt.widgets.Composite;
--- projects/ar.uba.dc.thesis.scenariosUI/src/scenariosui/test/ScenariosUIPropertiesVerifier.java
+++ projects/ar.uba.dc.thesis.scenariosUI/src/scenariosui/test/ScenariosUIPropertiesVerifier.java
@@ -1,19 +1,3 @@
-/*
- * Licencia de Caja de Valores S.A., Versión 1.0
- *
- * Copyright (c) 2006 Caja de Valores S.A.
- * 25 de Mayo 362, Ciudad Autónoma de Buenos Aires, República Argentina
- * Todos los derechos reservados.
- *
- * Este software es información confidencial y propietaria de Caja de Valores S.A. ("Información
- * Confidencial"). Usted no divulgará tal Información Confidencial y la usará solamente de acuerdo a
- * los términos del acuerdo de licencia que posee con Caja de Valores S.A.
- */
-
-/*
- * $Id: EmesPropertiesVerifier.java,v 1.8 2008/02/29 20:36:20 cvschioc Exp $
- */
-
 package scenariosui.test;
 
 import java.util.ResourceBundle;
@@ -28,11 +12,7 @@ import commons.test.PropertiesVerifier;
  * Verifica que todas las propiedades (incluyendo las commons, definidas en la librería) existan en ambos sentidos; esto
  * es, que todos los labels en los archivos properties estén definidos en las respectivas clases Enumeradas y que las
  * constantes definidas en estas últimas, estén definidas en los properties.
- * 
- * @author Jonathan Chiocchio
- * @version $Revision: 1.8 $ $Date: 2008/02/29 20:36:20 $
  */
-
 public class ScenariosUIPropertiesVerifier extends PropertiesVerifier {
 
 	@SuppressWarnings("unchecked")
