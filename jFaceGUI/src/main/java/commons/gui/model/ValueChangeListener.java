4eff8f4da2fbc3606de55ab5b2b748b0ca47b4aa
VisualSVN Server <VisualSVN Server@b0b93765-f1f9-fdba-7a1c-48fe7a44355a>
2010-08-09 07:12:58 +0000

    Deleted folder '/jFaceGUI'.

--- jFaceGUI/src/main/java/commons/gui/model/ValueChangeListener.java
+++
@@ -1,9 +0,0 @@
-package commons.gui.model;
-
-/**
- * Listener para ValueModel
- */
-public interface ValueChangeListener<T> {
-
-	public void valueChange(ValueChangeEvent<T> ev);
-}
--- jFaceGUI/src/main/java/commons/gui/model/collection/CollectionChangeListener.java
+++
@@ -1,9 +0,0 @@
-package commons.gui.model.collection;
-
-/**
- * @author P.Pastorino
- * 
- */
-public interface CollectionChangeListener {
-	public void collectionChange(CollectionChangeEvent ev);
-}
