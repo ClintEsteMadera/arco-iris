a8fdef0b9a9187e80e0078470349e8d7dbe810f6
Jonathan Chiocchio <jchiocchio@users.noreply.github.com>
2010-08-09 07:55:36 +0000

    Mi intención era poder buildear todo con Maven y dejarnos de joder con cosas raras pero el principal obstáculo que vi (y con el cual decidí no perder más tiempo) fue el hecho de que nuestros objetos del dominio están bastante contaminados con referencias a clases de Rainbow, clases que están en proyectos que, a priori, no es so easy de tirar en el repo de maven...en fin...me pudrí y solo separé lo nuestro del framework generico y ahora en los 3 proyectos usamos la misma convención (src/(main-test)/(java-resources) etc...)

--- projects/ar.uba.dc.thesis.scenariosUI/src/commons/gui/model/binding/ValueBindingUpdateListener.java
+++
@@ -1,6 +0,0 @@
-package commons.gui.model.binding;
-
-public interface ValueBindingUpdateListener {
-
-	public void updatingValueBinding(ValueBindingUpdateEvent e);
-}
--- projects/ar.uba.dc.thesis.scenariosUI/src/commons/gui/widget/group/IdentifiableElement.java
+++
@@ -1,7 +0,0 @@
-package commons.gui.widget.group;
-
-public interface IdentifiableElement<T> {
-	T getModel();
-
-	String getDescription();
-}
