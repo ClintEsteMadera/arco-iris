e80846a1cb2f40f1c70445e4ca194e68f1b35780
turco <turco@b0b93765-f1f9-fdba-7a1c-48fe7a44355a>
2010-10-27 17:54:33 +0000

    Cambios en los casos practicos para el informe

--- projects/ar.uba.dc.thesis/configs/znewsWithScenarios2/stitch/newssiteStrategies.s
+++ projects/ar.uba.dc.thesis/configs/znewsWithScenarios2/stitch/newssiteStrategies.s
@@ -12,31 +12,14 @@ define boolean RESP_TIME_STILL_BROKEN = AdaptationManagerWithScenarios.isConcern
 define boolean COST_STILL_BROKEN = AdaptationManagerWithScenarios.isConcernStillBroken("SERVER_COST");
 
 /* This Strategy is simple in that, while it encounters any anomaly in
- * experienced response time, it firsts enlists one new server, then lowers
- * fidelity one step, and quits
+ * experienced response time, it enlists one new server.
  *
  * Note:  Tested successfully in simulation, znews-varied
  */
-strategy EnlistServersResponseTime
+strategy EnlistServerResponseTime
 [ styleApplies ] {
   t0: (true) -> enlistServers(1) @[5000 /*ms*/] {
 	  t1: (!RESP_TIME_STILL_BROKEN) -> done;
-	  t2: (RESP_TIME_STILL_BROKEN) -> enlistServers(1) @[5000 /*ms*/] {
-		t3: (default) -> done;
-	  }
-	  t4: (default) -> TNULL;
-  }
-}
-
-/* This Strategy is triggered by the total server costs rising above acceptable
- * threshold; this Strategy reduces the number of active servers
- *
- * Note:  Tested successfully in simulation, znews-reducecost + znews-improvefidelity
- */
-strategy ReduceOverallCost
-[ styleApplies ] {
-  t0: (true) -> dischargeServers(1) @[5000 /*ms*/] {
-    t1: (!COST_STILL_BROKEN) -> done;
-    t2: (default) -> TNULL;
+	  t2: (default) -> TNULL;
   }
 }
