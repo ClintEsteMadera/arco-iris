b6b550bcc8416bd656b6c4ab40c38b86ae2e8b1d
gabrieltursi@gmail.com <gabrieltursi@gmail.com@b0b93765-f1f9-fdba-7a1c-48fe7a44355a>
2011-03-18 15:31:50 +0000

    Se agrega interface de ScenarioRelativePriorityAssigner

--- projects/ar.uba.dc.thesis/src/main/java/ar/uba/dc/thesis/selfhealing/ScenarioRelativePriorityAssigner.java
+++ projects/ar.uba.dc.thesis/src/main/java/ar/uba/dc/thesis/selfhealing/ScenarioRelativePriorityAssigner.java
@@ -1,18 +1,7 @@
 package ar.uba.dc.thesis.selfhealing;
 
-import ar.uba.dc.thesis.atam.scenario.SelfHealingConfigurationManager;
+public interface ScenarioRelativePriorityAssigner {
 
-public class ScenarioRelativePriorityAssigner {
-
-	private SelfHealingConfigurationManager selfHealingConfigurationManager;
-
-	public ScenarioRelativePriorityAssigner(SelfHealingConfigurationManager selfHealingConfigurationManager) {
-		this.selfHealingConfigurationManager = selfHealingConfigurationManager;
-	}
-
-	public int relativePriority(SelfHealingScenario scenario) {
-		int maxPriority = selfHealingConfigurationManager.getMaxPriority() + 1;
-		return (maxPriority - scenario.getPriority()) / maxPriority;
-	}
+	public abstract int relativePriority(SelfHealingScenario scenario);
 
 }
