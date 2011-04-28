d00ffb7beccc23ddb05068b1007f8db92456652d
jchiocchio <jchiocchio@b0b93765-f1f9-fdba-7a1c-48fe7a44355a>
2011-04-28 21:36:14 +0000

    refactoring en jerarquía de poms

--- projects/jFaceGUI/lib/install_missing_artifacts.cmd
+++ projects/jFaceGUI/lib/install_missing_artifacts.cmd
@@ -9,15 +9,3 @@ mvn install:install-file -DgroupId=org.eclipse.swt.gtk.linux -DartifactId=x86_64
 mvn install:install-file -DgroupId=org.eclipse.swt.gtk.linux -DartifactId=x86 -Dversion=6.6.6 -Dpackaging=jar -Dfile=swt_jface/native-lib-linux32.jar
 mvn install:install-file -DgroupId=org.eclipse.ui -DartifactId=forms -Dversion=3.2.0.v20060602 -Dpackaging=jar -Dfile=swt_jface/forms.jar
 mvn install:install-file -DgroupId=org.vafada -DartifactId=swtcalendar -Dversion=0.5 -Dpackaging=jar -Dfile=swtcalendar.jar
-
-mvn install:install-file -DgroupId=org.eclipse.core -DartifactId=core_commands -Dversion=6.6.6 -Dpackaging=jar -Dfile=swt_jface/core_commands.jar
-mvn install:install-file -DgroupId=org.eclipse.core -DartifactId=equinox_common -Dversion=6.6.6 -Dpackaging=jar -Dfile=swt_jface/equinox_common.jar
-mvn install:install-file -DgroupId=org.eclipse -DartifactId=swt -Dversion=6.6.6 -Dpackaging=jar -Dfile=swt_jface/swt.jar
-mvn install:install-file -DgroupId=org.eclipse -DartifactId=jface -Dversion=6.6.6 -Dpackaging=jar -Dfile=swt_jface/jface.jar
-mvn install:install-file -DgroupId=org.eclipse -DartifactId=osgi -Dversion=6.6.6 -Dpackaging=jar -Dfile=swt_jface/osgi.jar
-mvn install:install-file -DgroupId=org.eclipse.swt.win32.win32 -DartifactId=x86_64 -Dversion=6.6.6 -Dpackaging=jar -Dfile=swt_jface/native-lib-win64.jar
-mvn install:install-file -DgroupId=org.eclipse.swt.win32.win32 -DartifactId=x86 -Dversion=6.6.6 -Dpackaging=jar -Dfile=swt_jface/native-lib-win32.jar
-mvn install:install-file -DgroupId=org.eclipse.swt.gtk.linux -DartifactId=x86_64 -Dversion=6.6.6 -Dpackaging=jar -Dfile=swt_jface/native-lib-linux64.jar
-mvn install:install-file -DgroupId=org.eclipse.swt.gtk.linux -DartifactId=x86 -Dversion=6.6.6 -Dpackaging=jar -Dfile=swt_jface/native-lib-linux32.jar
-mvn install:install-file -DgroupId=org.eclipse.ui -DartifactId=forms -Dversion=3.2.0.v20060602 -Dpackaging=jar -Dfile=swt_jface/forms.jar
-mvn install:install-file -DgroupId=org.vafada -DartifactId=swtcalendar -Dversion=0.5 -Dpackaging=jar -Dfile=swtcalendar.jar
