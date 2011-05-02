a3338df662e58eebf8105860ed6eac728614e66c
Jonathan Chiocchio <jchiocchio@users.noreply.github.com>
2011-05-02 00:58:39 +0000

    renombre de proyectos

--- projects/ar.uba.dc.thesis/configs/znewsWithScenarios0/stitch/newssiteStrategies.s
+++
@@ -1,8 +0,0 @@
-/*
- * Adaptation script for the News Site example.
- */
-
-module newssite.strategies;
-
-import lib "newssiteTactics.s";
-
--- projects/ar.uba.dc.thesis/src/main/java/ar/uba/dc/thesis/common/Identifiable.java
+++
@@ -1,8 +0,0 @@
-package ar.uba.dc.thesis.common;
-
-public interface Identifiable {
-
-	public Long getId();
-
-	public void setId(Long id);
-}
