de2f8fa73b3ddce1e61b57799236001f4529ae6a
gabriel <gtursi@users.noreply.github.com>
2011-05-17 16:25:21 +0000

    Correccion de calculo de prioridad relativa para que, con prioridad 1, la prioridad relativa del escenario sea el maximo posible, o sea 1 (100%)

--- projects/arco-iris/arco-iris-core/src/main/java/ar/uba/dc/arcoiris/selfhealing/priority/DefaultScenarioRelativePriorityAssigner.java
+++ projects/arco-iris/arco-iris-core/src/main/java/ar/uba/dc/arcoiris/selfhealing/priority/DefaultScenarioRelativePriorityAssigner.java
@@ -1,5 +1,8 @@
 package ar.uba.dc.arcoiris.selfhealing.priority;
 
+import org.sa.rainbow.util.RainbowLogger;
+import org.sa.rainbow.util.RainbowLoggerFactory;
+
 import ar.uba.dc.arcoiris.atam.scenario.SelfHealingConfigurationManager;
 import ar.uba.dc.arcoiris.selfhealing.SelfHealingScenario;
 
@@ -12,9 +15,14 @@ public class DefaultScenarioRelativePriorityAssigner implements ScenarioRelative
 	}
 
 	@Override
-	public int relativePriority(SelfHealingScenario scenario) {
-		int maxPriority = selfHealingConfigurationManager.getMaxPriority() + 1;
-		return (maxPriority - scenario.getPriority()) / maxPriority;
+	public double relativePriority(SelfHealingScenario scenario) {
+		double maxPriority = selfHealingConfigurationManager.getMaxPriority();
+		double relativePriority = (maxPriority - scenario.getPriority() + 1) / maxPriority;
+		logger.debug(String.format("Relative priority for scenario %s with priority %d and max priority %d: %f",
+				scenario.getName(), scenario.getPriority(), (int) maxPriority, relativePriority));
+		return relativePriority;
 	}
+
+	private static RainbowLogger logger = RainbowLoggerFactory.logger(SelfHealingConfigurationManager.class);
 
 }
