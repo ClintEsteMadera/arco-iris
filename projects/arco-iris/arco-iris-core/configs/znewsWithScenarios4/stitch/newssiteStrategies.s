4f2880007952c8c5ca7c2d662355a98a5fb88230
gabriel <gtursi@users.noreply.github.com>
2011-05-20 15:45:13 +0000

    Correccion y depuracion de las estrategias

--- projects/arco-iris/arco-iris-core/configs/znewsWithScenarios4/stitch/newssiteStrategies.s
+++ projects/arco-iris/arco-iris-core/configs/znewsWithScenarios4/stitch/newssiteStrategies.s
@@ -7,33 +7,16 @@ module newssite.strategies;
 import lib "newssiteTactics.s";
 
 define boolean styleApplies = Model.hasType(M, "ClientT") && Model.hasType(M, "ServerT");
-define boolean cViolation = exists c : T.ClientT in M.components | c.experRespTime > M.MAX_RESPTIME;
 
-define set servers = {select s : T.ServerT in M.components | true};
-define set unhappyClients = {select c : T.ClientT in M.components | c.experRespTime > M.MAX_RESPTIME};
-define int numClients = Set.size({select c : T.ClientT in M.components | true});
-define int numUnhappy = Set.size(unhappyClients);
-define float numUnhappyFloat = 1.0*numUnhappy;
+define boolean COST_STILL_BROKEN = ArcoIrisAdaptationManager.isConcernStillBroken("SERVER_COST");
+define boolean RESP_TIME_STILL_BROKEN = ArcoIrisAdaptationManager.isConcernStillBroken("RESPONSE_TIME");
 
-define boolean hiLoad = exists s : T.ServerT in M.components | s.load > M.MAX_UTIL;
-define boolean hiRespTime = exists c : T.ClientT in M.components | c.experRespTime > M.MAX_RESPTIME;
-define boolean lowRespTime = exists c : T.ClientT in M.components | c.experRespTime < M.MIN_RESPTIME;
-
-define float totalCost = Model.sumOverProperty("cost", servers);
-define boolean hiCost = totalCost >= M.THRESHOLD_COST;
-
-/*TO-DO: agregar logica en el ResponseMeasure para que soporte esto*/
-define float avgFidelity = Model.sumOverProperty("fidelity", servers) / Set.size(servers);
-define boolean lowFi = avgFidelity < M.THRESHOLD_FIDELITY;
-
-define boolean RESP_TIME_STILL_BROKEN = ArcoIrisAdaptationManagerncernStillBroken("RESPONSE_TIME");
-
-/* Esta estrategia repara el response time pero es muy costosa,
- * no deberia ser elegida ya que existe un escenario de Server Cost que se rompería.
+/* This strategy will enlist 2 servers
+ *
  */
 strategy ExpensiveReduceResponseTime
 [ styleApplies ] {
-  t0: (true) -> enlistServers(4) @[2000 /*ms*/]{
+  t0: (true) -> enlistServers(2) @[2000 /*ms*/]{
   	t1: (default) -> TNULL;
   }
 }
